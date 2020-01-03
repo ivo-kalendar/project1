78964e4c4044483f0cead8b2fadc80a466c6f71e
Ivo <ivokalendar@icloud.com>
2020-01-03 10:51:50 +0100

    Completed adding middleware.

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 
+const express = require('express')
+const app = express()
 
-console.log('hello')
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.listen(3000)
+
+let obj = [
+	{
+		"name": "ivo",
+		"surname": "kalendar",
+		"addres" : "ul.Lazar Pop Trajkov"
+	},{
+		"name": "ivo",
+		"surname": "kalendar",
+		"addres" : "ul.Lazar Pop Trajkov"
+	},{
+		"name": "ivo",
+		"surname": "kalendar",
+		"addres" : "ul.Lazar Pop Trajkov"
+	}
+]
+
+app.get('/', (req,res)=>{
+	res.send(obj)
+})
